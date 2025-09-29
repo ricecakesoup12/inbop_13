cfc97e0dc2483eb717707eb1edcbf6796838cb06
ricecakesoup12 <rlatjrd456@naver.com>
2025-09-29 15:29:05 +0900

    first commit

---
+++ src/main/java/inbop/_group/Controller/TestController.java
@@ -0,0 +1,11 @@
+package inbop._group.Controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public TestController() {
+        System.out.println("controller 생성");
+    }
+}
